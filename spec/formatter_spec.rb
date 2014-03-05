7face3eed3711959931e2bbf60368fc4eb558bbe
Jingwen Owen Ou <jingweno@gmail.com>
2014-03-04 21:20:53 -0800

    Introduce fixtures for spec

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -3,12 +3,15 @@ require "rbfmt/formatter"
 
 describe Rbfmt::Formatter do
   it "formats source" do
-    formatter = described_class.new('def hello(world) "Hello, #{world}!"; end')
-    formatted_source = <<-EOS
-def hello(world)
-  "Hello, \#\{world\}!"
-end
-EOS
-    formatter.format.should == formatted_source
+    fixtures = Dir.glob File.join(File.expand_path(File.dirname(__FILE__)), "fixtures", "**", "*.rb")
+    formatted_fixtures = fixtures.select { |f| f[/.+_formatted.rb/] }
+    source_fixtures = fixtures - formatted_fixtures
+
+    source_fixtures.each do |s|
+      filename =  File.basename(s, ".rb")
+      formatted = formatted_fixtures.detect { |f| f[/#{filename}_formatted.rb/] }
+      fail "No formatted fixture for #{s}" unless formatted
+      described_class.new(File.read(s)).format.should == File.read(formatted)
+    end
   end
 end
